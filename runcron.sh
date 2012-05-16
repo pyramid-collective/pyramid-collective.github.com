c14468ede723de84c2e8fd9f34c4b01d544e1836
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2012-05-17 00:57:14 +0200

    reset history

--- runcron.sh
+++ runcron.sh
@@ -4,3 +4,8 @@
 /opt/minitage/pyramidcollective/venv/bin/github-collective  -o pyramid-collective -u kiorky -P secret -c https://raw.github.com/pyramid-collective/pyramid-collective.github.com/master/permissions.cfg
 
 # vim:set et sts=4 ts=4 tw=0:
+
+
+
+
+
